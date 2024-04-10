d41049233ed6f2f62b0ed70378e10d04085100b7
Shruti-Shinde22 <147103536+Shruti-Shinde22@users.noreply.github.com>
2024-04-11 00:44:28 +0530

    Update instance.js

--- client/src/axios/instance.js
+++ client/src/axios/instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const isDev = true;
 
-const url = isDev ? "password-manager-app-delta.vercel.app" : "";
+const url = isDev ? "https://password-manager-app-delta.vercel.app/" : "";
 
 const instance = axios.create({
     baseURL: url,
